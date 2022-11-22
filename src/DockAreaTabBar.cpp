89e0062dfc9040258ac9ae3a12134f56ba9c31ef
”ZiluoLiu-LXKJ” <370376734@qq.com>
2022-11-22 08:34:20 +0800

    Optimized the tabbar style

--- src/DockAreaTabBar.cpp
+++ src/DockAreaTabBar.cpp
@@ -124,7 +124,7 @@ CDockAreaTabBar::CDockAreaTabBar(CDockAreaWidget* parent) :
 {
 	d->DockArea = parent;
 	setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
-//	setFrameStyle(QFrame::NoFrame);
+    setFrameStyle(QFrame::NoFrame);
 	setWidgetResizable(true);
 	setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 	setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
